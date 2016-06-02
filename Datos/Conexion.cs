492abd858790d4a401b890a53dc957281ef149c2
crisrc012 <crisrc012@hotmail.com>
2016-06-01 23:49:36 -0600

    Conexión para SQL Server y MySQL

--- Datos/Conexion.cs
+++ Datos/Conexion.cs
@@ -8,21 +8,50 @@ namespace Datos
         private SqlConnection conexionMSSQL;
         private MySqlConnection conexionMySQL;
         private string cadenaConexion;
+        private bool mysql;
 
-        public Conexion()
+        public Conexion(bool mysql = false)
         {
-            cadenaConexion = @"Data Source = localhost\SQLEXPRESS; Initial Catalog = ProyectoFinal; Integrated Security = true;";
-            conexionMSSQL = new SqlConnection(cadenaConexion);
+            this.mysql = mysql;
+            string servidor = "localhost";
+            string bd = "ProyectoFinal";
+            string usuario = "";
+            string contrasena = "";
+            if (!mysql)
+            {
+                cadenaConexion = "Data Source = " + servidor + @"\SQLEXPRESS; Initial Catalog = " + bd + "; User ID=" + usuario + ";Password=" + contrasena + ";";
+                conexionMSSQL = new SqlConnection(cadenaConexion);
+            }
+            else
+            {
+                cadenaConexion = "SERVER = " + servidor + ";" + "DATABASE = " + bd + ";" + "UID = " + usuario + ";" + "PASSWORD = " + contrasena + ";";
+                conexionMySQL = new MySqlConnection(cadenaConexion);
+            }
         }
 
         public SqlConnection getMSSQLConnection()
         {
-            return conexionMSSQL;
+            if (!this.mysql)
+            {
+                return conexionMSSQL;
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public MySqlConnection getMySQLConnection()
         {
-            return conexionMySQL;
+            if (this.mysql)
+            {
+                return conexionMySQL;
+            }
+            else
+            {
+                return null;
+            }
+            
         }
     }
 }
