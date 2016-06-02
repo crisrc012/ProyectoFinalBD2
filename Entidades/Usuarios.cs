6b530cce72b1758ebe6eeec4ab927650d8f02c67
crisrc012 <crisrc012@hotmail.com>
2016-06-02 00:01:23 -0600

    Entidades

---
+++ Entidades/Usuarios.cs
@@ -0,0 +1,10 @@
+﻿namespace Entidades
+{
+    public class Usuarios
+    {
+        public int id_usuarios { get; set; }
+        public string nombre { get; set; }
+        public string apellido1 { get; set; }
+        public string apellido2 { get; set; }
+    }
+}
