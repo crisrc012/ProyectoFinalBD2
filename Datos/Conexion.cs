c515b1efcd31a46881b67099f6c6b5ab8be5a866
crisrc012 <crisrc012@hotmail.com>
2016-06-01 23:32:24 -0600

    Proyecto Inicial

---
+++ Datos/Conexion.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace Datos
+{
+    public class Conexion
+    {
+        private SqlConnection conexionMSSQL;
+        private MySqlConnection conexionMySQL;
+        private string cadenaConexion;
+
+        public Conexion()
+        {
+            cadenaConexion = @"Data Source = localhost\SQLEXPRESS; Initial Catalog = ProyectoFinal; Integrated Security = true;";
+            conexionMSSQL = new SqlConnection(cadenaConexion);
+        }
+
+        public SqlConnection getMSSQLConnection()
+        {
+            return conexionMSSQL;
+        }
+
+        public MySqlConnection getMySQLConnection()
+        {
+            return conexionMySQL;
+        }
+    }
+}
---
+++ ProyectoFinalBD2/Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProyectoFinalBD2
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
