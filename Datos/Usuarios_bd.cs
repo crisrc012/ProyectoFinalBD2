94c8dbada74039deddca0f2c53ac1ccfa1fcc5f1
crisrc012 <crisrc012@hotmail.com>
2016-06-06 22:35:59 -0600

    Insert

--- Datos/Usuarios_bd.cs
+++ Datos/Usuarios_bd.cs
@@ -18,7 +18,7 @@ namespace Datos
         {
             string sql = "select id_usuarios,nombre,apellido1,apellido2 from usuarios;";
             DataTable datos = new DataTable();
-            if (c.getConexion().conexionMSSQL == null)
+            if (c.getConexion().conexionMSSQL != null)
             {
                 SqlCommand cmd = new SqlCommand(sql, c.getConexion().conexionMSSQL);
                 SqlDataAdapter adapter = new SqlDataAdapter(cmd);
@@ -35,7 +35,54 @@ namespace Datos
 
         public void Insert(Usuarios u)
         {
-
+            string sql = "insert into usuarios (id_usuarios,nombre,apellido1,apellido2) "
+                    + "values(@id_usuarios, @nombre, @apellido1, @apellido2);";
+            if (c.getConexion().conexionMSSQL != null)
+            {
+                c.getConexion().conexionMSSQL.Open();
+                SqlCommand cmd = new SqlCommand(null, c.getConexion().conexionMSSQL);
+                cmd.CommandText = sql;
+                cmd.Parameters.Add("@id_usuarios", SqlDbType.Int).Value = u.id_usuarios;
+                cmd.Parameters.Add("@nombre", SqlDbType.VarChar).Value = u.nombre;
+                cmd.Parameters.Add("@apellido1", SqlDbType.VarChar).Value = u.apellido1;
+                cmd.Parameters.Add("@apellido2", SqlDbType.VarChar).Value = u.apellido2;
+                cmd.Prepare();
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    System.Console.Write(ex.Message);
+                }
+                finally
+                {
+                    c.getConexion().conexionMSSQL.Close();
+                }
+            }
+            else
+            {
+                c.getConexion().conexionMySQL.Open();
+                MySqlCommand cmd = new MySqlCommand(null, c.getConexion().conexionMySQL);
+                cmd.CommandText = sql;
+                cmd.Parameters.AddWithValue("@id_usuarios", SqlDbType.Int).Value = u.id_usuarios;
+                cmd.Parameters.AddWithValue("@nombre", SqlDbType.VarChar).Value = u.nombre;
+                cmd.Parameters.AddWithValue("@apellido1", SqlDbType.VarChar).Value = u.apellido1;
+                cmd.Parameters.AddWithValue("@apellido2", SqlDbType.VarChar).Value = u.apellido2;
+                cmd.Prepare();
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (MySqlException ex)
+                {
+                    System.Console.Write(ex.Message);
+                }
+                finally
+                {
+                    c.getConexion().conexionMySQL.Close();
+                }
+            }
         }
 
         public void Update(Usuarios u)
