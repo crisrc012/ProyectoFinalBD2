642f7278c2f73e11a09d57193a53c9496a8040a5
crisrc012 <crisrc012@hotmail.com>
2016-06-06 22:49:57 -0600

    Logica

--- Logica/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Logica
-{
-    public class Class1
-    {
-    }
-}
---
+++ Logica/Usuarios_lg.cs
@@ -0,0 +1,35 @@
+﻿using Datos;
+using Entidades;
+using System.Data;
+
+namespace Logica
+{
+    public class Usuarios_lg
+    {
+        private Usuarios_bd ubd;
+
+        public DataTable Select()
+        {
+            ubd = new Usuarios_bd();
+            return ubd.Select();
+        }
+
+        public void Insert(Usuarios u)
+        {
+            ubd = new Usuarios_bd();
+            ubd.Insert(u);
+        }
+
+        public void Update(Usuarios u)
+        {
+            ubd = new Usuarios_bd();
+            ubd.Update(u);
+        }
+
+        public void Delete(int id)
+        {
+            ubd = new Usuarios_bd();
+            ubd.Delete(id);
+        }
+    }
+}
