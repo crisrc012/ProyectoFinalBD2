de4d118bd3f28c6ccd6372ebfdf136289196266b
ajfallasf <arturo12_97@hotmail.com>
2016-07-06 15:20:22 -0600

    Insertando usuarios a SQL

---
+++ ProyectoFinalBD2/InsertarUsuario.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace ProyectoFinalBD2
+{
+    partial class InsertarUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.txtIdUsuario = new System.Windows.Forms.TextBox();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.cmMotores = new System.Windows.Forms.ComboBox();
+            this.lblMotor = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(205, 376);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(137, 66);
+            this.btnInsertar.TabIndex = 0;
+            this.btnInsertar.Text = "Insertar Usuario";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cedula";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID Usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Contrasena";
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(238, 104);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(100, 22);
+            this.txtCedula.TabIndex = 4;
+            // 
+            // txtIdUsuario
+            // 
+            this.txtIdUsuario.Location = new System.Drawing.Point(238, 142);
+            this.txtIdUsuario.Name = "txtIdUsuario";
+            this.txtIdUsuario.Size = new System.Drawing.Size(100, 22);
+            this.txtIdUsuario.TabIndex = 5;
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Location = new System.Drawing.Point(238, 252);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.Size = new System.Drawing.Size(100, 22);
+            this.txtContrasena.TabIndex = 6;
+            // 
+            // cmMotores
+            // 
+            this.cmMotores.FormattingEnabled = true;
+            this.cmMotores.Items.AddRange(new object[] {
+            "MySQL",
+            "SQL"});
+            this.cmMotores.Location = new System.Drawing.Point(238, 324);
+            this.cmMotores.Name = "cmMotores";
+            this.cmMotores.Size = new System.Drawing.Size(121, 24);
+            this.cmMotores.TabIndex = 7;
+            // 
+            // lblMotor
+            // 
+            this.lblMotor.AutoSize = true;
+            this.lblMotor.Location = new System.Drawing.Point(60, 331);
+            this.lblMotor.Name = "lblMotor";
+            this.lblMotor.Size = new System.Drawing.Size(44, 17);
+            this.lblMotor.TabIndex = 8;
+            this.lblMotor.Text = "Motor";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(72, 191);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(127, 17);
+            this.lblUsername.TabIndex = 9;
+            this.lblUsername.Text = "nombre de usuario";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(238, 188);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 22);
+            this.txtUsername.TabIndex = 10;
+            // 
+            // InsertarUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 470);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lblMotor);
+            this.Controls.Add(this.cmMotores);
+            this.Controls.Add(this.txtContrasena);
+            this.Controls.Add(this.txtIdUsuario);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnInsertar);
+            this.Name = "InsertarUsuario";
+            this.Text = "InsertarUsuario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.TextBox txtIdUsuario;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.ComboBox cmMotores;
+        private System.Windows.Forms.Label lblMotor;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.TextBox txtUsername;
+    }
+}
---
+++ ProyectoFinalBD2/InsertarUsuario.cs
@@ -0,0 +1,67 @@
+﻿using Datos;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinalBD2
+{
+    public partial class InsertarUsuario : Form
+    {
+        public Conexion c;
+        public Usuarios_bd usersbd;
+        public InsertarUsuario()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            if (cmMotores.SelectedItem == null)
+            {
+                MessageBox.Show(this,
+                    "Debe de seleccionar un motor de base de datos",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+            txtCedula.Text = "";
+            // ejecutando segun motor
+            if (cmMotores.SelectedItem.ToString() == "SQL")
+            {
+                string query = "Select * from Persona where Cedula='" + txtCedula + "'";
+                SqlCommand cmd = new SqlCommand(query, c.getConexion().conexionMSSQL);
+                SqlDataReader dtr = cmd.ExecuteReader();
+                if (dtr.HasRows)
+                {
+                    Usuarios nuevo = new Usuarios();
+                    nuevo.cedula = Int32.Parse(txtCedula.Text);
+                    nuevo.contraseña = txtContrasena.Text;
+                    nuevo.id_rol = 1;
+                    nuevo.id_usuarios = Int32.Parse(txtIdUsuario.Text);
+                    nuevo.username = txtUsername.Text;
+                    usersbd.Insert(nuevo);
+                }
+        
+
+            } else
+            {
+
+            }
+            
+
+
+
+
+        }
+    }
+}
