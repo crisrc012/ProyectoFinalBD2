a3d76d175472a45d9229c17ef3c5eed680ee4879
Cristopher Robles Ríos <crisrc012@hotmail.com>
2016-07-26 23:20:44 -0600

    Cambio de ventana

--- ProyectoFinalBD2/Program.cs
+++ ProyectoFinalBD2/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ProyectoFinalBD2
@@ -16,7 +13,7 @@ namespace ProyectoFinalBD2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Principal());
         }
     }
 }
