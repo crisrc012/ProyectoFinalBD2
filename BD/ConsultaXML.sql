c772fdd11cb0a618eea56b2bf24a1f3dbdfa32d2
pagui97 <paguimendez97@gmail.com>
2016-07-27 01:35:54 -0600

    Pequeño cambio

--- BD/ConsultaXML.sql
+++ BD/ConsultaXML.sql
@@ -6,44 +6,44 @@ BEGIN
 	SET NOCOUNT ON;
 	SELECT F.id_factura AS IdFactura
 	, F.fecha AS Fecha
-	,(SELECT P.Nombre + P.apellido1 + P.apellido2 AS Nombre
-	FROM persona P
-	INNER JOIN cliente C
+	,(SELECT [dbo].[nombre_completo](P.cedula) AS Nombre
+	FROM [ProyectoFinal].[dbo].[persona] P
+	INNER JOIN [ProyectoFinal].[dbo].[cliente] C
 	ON P.cedula = C.cedula
-	INNER JOIN factura F
+	INNER JOIN [ProyectoFinal].[dbo].[factura] F
 	ON F.id_cliente = C.id_cliente
 	AND F.id_factura = @id_factura
 	FOR XML AUTO, TYPE) AS Cliente
-	,(SELECT P.Nombre + P.apellido1 + P.apellido2 AS Nombre
-	FROM persona P
-	INNER JOIN vendedor V
+	,(SELECT [dbo].[nombre_completo](P.cedula) AS Nombre
+	FROM [ProyectoFinal].[dbo].[persona] P
+	INNER JOIN [ProyectoFinal].[dbo].[vendedor]  V
 	ON P.cedula = V.cedula
-	INNER JOIN factura F
+	INNER JOIN [ProyectoFinal].[dbo].[factura] F
 	ON F.id_vendedor = V.id_vendedor
 	AND F.id_factura = @id_factura
 	FOR XML AUTO, TYPE) AS Vendedor
-	,(SELECT P.Nombre + P.apellido1 + P.apellido2 AS Nombre
-	FROM persona P
-	INNER JOIN proveedor PV
+	,(SELECT [dbo].[nombre_completo](P.cedula) AS Nombre
+	FROM [ProyectoFinal].[dbo].[persona] P
+	INNER JOIN [ProyectoFinal].[dbo].[proveedor] PV
 	ON P.cedula = PV.cedula
-	INNER JOIN producto Pr
+	INNER JOIN [ProyectoFinal].[dbo].[producto] Pr
 	ON Pr.id_proveedor = PV.id_proveedor
-	INNER JOIN detalle D
+	INNER JOIN [ProyectoFinal].[dbo].[detalle] D
 	ON D.id_producto = Pr.id_producto
 	AND D.id_factura = @id_factura
 	FOR XML AUTO, TYPE) AS Proveedor
 	,(SELECT Pr.nombre AS Producto
 	, D.descuento AS Descuento
 	, D.cantidad AS Cantidad 
-	FROM detalle D
-	INNER JOIN producto Pr
+	FROM [ProyectoFinal].[dbo].[detalle] D
+	INNER JOIN [ProyectoFinal].[dbo].[producto] Pr
 	ON D.id_producto = Pr.id_producto
 	AND D.id_factura = @id_factura
 	FOR XML AUTO, TYPE) AS Detalle 
 	, F.subtotal AS Subtotal
 	, F.impuesto AS Impuesto
 	, F.total AS Total
-	FROM factura F
+	FROM [ProyectoFinal].[dbo].[factura] F
 	WHERE F.id_factura = @id_factura
 	FOR XML AUTO, ELEMENTS
 
