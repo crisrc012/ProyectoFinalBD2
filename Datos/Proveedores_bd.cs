5cf545bbeda4447dff78647447977c0faf03d282
Cristopher Robles Ríos <crisrc012@hotmail.com>
2016-07-27 01:42:29 -0600

    Proveedores BD

--- Datos/Proveedores_bd.cs
+++ Datos/Proveedores_bd.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Datos
 {
-    class Proveedores_bd
+    public class Proveedores_bd
     {
+        private Conexion c;
+        bool mysql;
+
+        public Proveedores_bd(bool mysql = false)
+        {
+            this.mysql = mysql;
+            c = new Conexion(mysql);
+        }
+
+        public DataTable Select(int? id_proveedor = null, int? cedula = null, int? id_compania = null, string cargo = null)
+        {
+            string sql = "EXEC sp_proveedor @id_proveedor, @cedula, @id_compania, @cargo;";
+
+            DataTable datos = new DataTable();
+            if (!mysql)
+            {
+                c.getConexion().conexionMSSQL.Open();
+                SqlCommand cmd = new SqlCommand(sql, c.getConexion().conexionMSSQL);
+                cmd.Parameters.Add("@id_proveedor", SqlDbType.Int).Value = (object)id_proveedor ?? DBNull.Value;
+                cmd.Parameters.Add("@cedula", SqlDbType.Int).Value = (object)cedula ?? DBNull.Value;
+                cmd.Parameters.Add("@id_compania", SqlDbType.Int).Value = (object)id_compania ?? DBNull.Value;
+                cmd.Parameters.Add("@cargo", SqlDbType.Int).Value = (object)cargo ?? DBNull.Value;
+                cmd.Prepare();
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(datos);
+            }
+            else
+            {
+                MySqlCommand cmd = new MySqlCommand(sql, c.getConexion().conexionMySQL);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                adapter.Fill(datos);
+            }
+            return datos;
+        }
     }
 }
