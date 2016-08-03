983aca5db4412cbcd7212f90e3e4d4fb84690a11
Cristopher Robles Ríos <crisrc012@hotmail.com>
2016-08-03 01:01:15 -0600

    Facturas MySQL y MSSQL

--- ProyectoFinalBD2/Selects.cs
+++ ProyectoFinalBD2/Selects.cs
@@ -116,11 +116,16 @@ namespace ProyectoFinalBD2
                 return;
             }
             Clientes cl = new Clientes();
-            cl.id_cliente = int.Parse(dgvSelects.Rows[dgvSelects.SelectedRows[0].Index].Cells[0].Value.ToString());
-            cl.cedula = int.Parse(dgvSelects.Rows[dgvSelects.SelectedRows[0].Index].Cells[1].Value.ToString());
-            cl.nombre = dgvSelects.Rows[dgvSelects.SelectedRows[0].Index].Cells[2].Value.ToString();
-            cl.apellido1 = dgvSelects.Rows[dgvSelects.SelectedRows[0].Index].Cells[3].Value.ToString();
-            cl.apellido2 = dgvSelects.Rows[dgvSelects.SelectedRows[0].Index].Cells[4].Value.ToString();
+            cl.id_cliente = 
+                int.Parse(dgvSelects.Rows[dgvSelects.SelectedRows[0].Index].Cells[0].Value.ToString());
+            cl.cedula = 
+                int.Parse(dgvSelects.Rows[dgvSelects.SelectedRows[0].Index].Cells[1].Value.ToString());
+            cl.nombre = 
+                dgvSelects.Rows[dgvSelects.SelectedRows[0].Index].Cells[2].Value.ToString();
+            cl.apellido1 = 
+                dgvSelects.Rows[dgvSelects.SelectedRows[0].Index].Cells[3].Value.ToString();
+            cl.apellido2 = 
+                dgvSelects.Rows[dgvSelects.SelectedRows[0].Index].Cells[4].Value.ToString();
             Modificar_Cliente mdf = new Modificar_Cliente(cl,cbMotor.SelectedItem.ToString());
             mdf.MdiParent = MdiParent;
             mdf.Show();
@@ -128,7 +133,17 @@ namespace ProyectoFinalBD2
 
         private void btnClFacturas_Click(object sender, EventArgs e)
         {
-
+            if (dgvSelects.SelectedRows.Count != 1)
+            {
+                MessageBox.Show(this, 
+                    "Debe de seleccionar únicamente un cliente", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            VerFacturas mdf = 
+                new VerFacturas(int.Parse(dgvSelects.Rows[dgvSelects.SelectedRows[0].Index].Cells[1].Value.ToString()), 
+                cbMotor.SelectedItem.ToString());
+            mdf.MdiParent = MdiParent;
+            mdf.Show();
         }
     }
 }
--- ProyectoFinalBD2/VerFacturas.cs
+++ ProyectoFinalBD2/VerFacturas.cs
@@ -5,15 +5,22 @@ namespace ProyectoFinalBD2
 {
     public partial class VerFacturas : Form
     {
-        public VerFacturas()
+        private int? cedula;
+        private bool mysql = false;
+        public VerFacturas(int? cedula = null, string cb = null)
         {
             InitializeComponent();
+            this.cedula = cedula;
+            if (cb == "MySQL")
+            {
+                mysql = true;
+            }
         }
 
         private void VerFacturas_Load(object sender, EventArgs e)
         {
-            Logica.Factura_lg clg = new Logica.Factura_lg(false);
-            dGV.DataSource = clg.Select();
+            Logica.Factura_lg clg = new Logica.Factura_lg(mysql);
+            dGV.DataSource = clg.Select(null,null,cedula);
         }
     }
 }
